f9b2ce39017cc03d5e7e06d5c2a02b04b9e269fe
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-08-21 17:22:20 +0200

    Update and rename Installskript.txt to install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash  
+echo "Bitte die Hinweise im Git-Repository beachten!"
+
+sudo apt update;
+sudo apt upgrade;
+sudo apt install -y git;
+sudo apt install -y nginx;
+sudo apt install -y python3;
+sudo apt install -y certbot;
+sudo apt install -y python-certbot-nginx;
+sudo apt install -y ruby;
+sudo apt install -y default-jdk;
+sudo apt install -y default-jre;
+sudo apt install -y python3-pip;
+sudo pip install -y flask;
+sudo shutdown -r now;
