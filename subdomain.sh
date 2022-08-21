5b9cc5a84016c33fd11469a8e09157be2f629b10
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-08-21 18:15:19 +0200

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 echo "Bitte gib die Subdomnain ein, die du einrichten möchtest (z.B. testserver.stichels.de)."
 read -p "Subdomain: " subdomain;
+echo "Bitte gib den Port, auf dem dein Projekt laufen wird, ein (z.B. 8080)."
+read -p "Port: " port;
+rm /etc/nginx/sites-enabled/$subdomain;
+echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
+echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
+echo "proxy_pass      http://127.0.0.1:$port;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "}" >> /etc/nginx/sites-enabled/$subdomain;
+echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 sudo certbot --nginx -d $subdomain;
 systemctl restart nginx;
