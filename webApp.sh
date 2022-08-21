186d01fd3f36f6a00f96732c55f0fa8ea45ab7c5
Ivo Stichel <ivostichel@outlook.com>
2022-08-21 17:08:17 +0000

    Bug

--- webApp.sh
+++ webApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Bitte gib die Subdomnain ein, die du für die Beispiel webApp eingerichtet hast."
 read -p "Subdomain: " subdomain;
-sed -i 's/webapp.stichels.de/$subdomain/g' Infrastruktur-Einrichtung/webApp/index.html
+sed -i "s/webapp.stichels.de/$subdomain/g" Infrastruktur-Einrichtung/webApp/index.html
 cd Infrastruktur-Einrichtung/webApp;
 flask run;
