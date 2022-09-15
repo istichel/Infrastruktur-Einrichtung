b4eada83fd97d31cdffb8d5dbf60750e60799c28
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-15 08:25:47 +0200

    Create installgitea.sh

---
+++ installgitea.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update;
+sudo apt upgrade;
+sudo apt install sqlite;
+wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+chmod +x gitea;
+adduser --system --shell /bin/bash --gecos 'Git Version Control' --group --disabled-password --home /home/git git;
+mkdir -p /var/lib/gitea/{custom,data,log};
+chown -R git:git /var/lib/gitea/;
+chmod -R 750 /var/lib/gitea/;
+mkdir /etc/gitea;
+chown root:git /etc/gitea
+chmod 770 /etc/gitea
+export GITEA_WORK_DIR=/var/lib/gitea/
+cp gitea /usr/local/bin/gitea
+
