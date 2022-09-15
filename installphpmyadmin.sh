fab60889ab1a283ee3e673f03098e7dfe22479a2
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-15 13:41:57 +0200

    Update installphpmyadmin.sh

--- installphpmyadmin.sh
+++ installphpmyadmin.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+echo "Bitte gib die Subdomnain für gitea ein."
+read -p "Subdomain: " subdomain;
+rm /etc/nginx/sites-enabled/$subdomain;
+echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
+echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
+echo "proxy_pass      http://127.0.0.1:8000;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "}" >> /etc/nginx/sites-enabled/$subdomain;
+echo "}" >> /etc/nginx/sites-enabled/$subdomain;
+sudo certbot --nginx -d $subdomain;
+pkill nginx;
+systemctl start nginx;
+sudo apt -y update;
+sudo apt -y install phpmyadmin php-mbstring php-zip php-gd php-json php-curl;
