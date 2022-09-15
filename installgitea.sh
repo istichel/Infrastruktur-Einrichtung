116ed8b4740834553c5ce849ea5959b21c164855
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-15 09:46:18 +0200

    Update installgitea.sh

--- installgitea.sh
+++ installgitea.sh
@@ -5,7 +5,7 @@ rm /etc/nginx/sites-enabled/$subdomain;
 echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
 echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
-echo "proxy_pass      http://127.0.0.1:999;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "proxy_pass      http://127.0.0.1:3000;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 sudo certbot --nginx -d $subdomain;
