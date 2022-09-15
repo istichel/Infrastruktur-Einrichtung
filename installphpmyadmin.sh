6fc2084fbb44df946a8857f98233cbbf0589c733
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-15 15:02:03 +0200

    Update installphpmyadmin.sh

--- installphpmyadmin.sh
+++ installphpmyadmin.sh
@@ -6,6 +6,7 @@ echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
 echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
 echo "proxy_pass      http://127.0.0.1:81;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "proxy_set_header X-Forwarded-Proto https;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 sudo certbot --nginx -d $subdomain;
