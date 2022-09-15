df10dcd6ef79741ec6090d84f193b248cd87fce0
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-15 08:30:31 +0200

    Update installgitea.sh

--- installgitea.sh
+++ installgitea.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#!/bin/bash
+echo "Bitte gib die Subdomnain für gitea ein."
+read -p "Subdomain: " subdomain;
+rm /etc/nginx/sites-enabled/$subdomain;
+echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
+echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
+echo "proxy_pass      http://127.0.0.1:999;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "}" >> /etc/nginx/sites-enabled/$subdomain;
+echo "}" >> /etc/nginx/sites-enabled/$subdomain;
+sudo certbot --nginx -d $subdomain;
+pkill nginx;
+systemctl start nginx;
 sudo apt update;
 sudo apt upgrade;
 sudo apt install sqlite;
