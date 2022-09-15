cb5b3a8298cf5100beb94b79a3ca983d9ea8eb9f
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-15 14:24:20 +0200

    Update installphpmyadmin.sh

--- installphpmyadmin.sh
+++ installphpmyadmin.sh
@@ -7,7 +7,7 @@ rm /etc/nginx/sites-enabled/$subdomain;
 echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
 echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
-echo "proxy_pass      http://127.0.0.1:8000;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "proxy_pass      http://127.0.0.1:81;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 echo "}" >> /etc/nginx/sites-enabled/$subdomain;
 sudo certbot --nginx -d $subdomain;
@@ -19,6 +19,6 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY
 sudo mysql_secure_installation;
 sed -i s/80/81/g /etc/apache2/ports.conf;
 sed -i s/443/444/g /etc/apache2/ports.conf;
-sed -i s/'/var/www/html'/'/var/www/html/phpmyadmin'/g /etc/apache2/ports.conf;
+sed -i s/'\/var\/www\/html'/'\/var\/www\/html\/phpmyadmin'/g /etc/apache2/ports.conf;
 sudo apt -y install php libapache2-mod-php php-mysql;
 sudo apt -y install phpmyadmin php-mbstring php-zip php-gd php-json php-curl;
