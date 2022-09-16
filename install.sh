561ab902d02c1f1dab17062a1628afd49a230fbb
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2022-09-16 12:43:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 echo "Bitte die Hinweise im Git-Repository beachten!"
 sudo apt update -y;
 sudo apt install -y needrestart;
-sed -i s/"#\$nrconf{restart} = 'i';"/"\$nrconf{restart} = 'a';"/g needrestart.conf;
-sed -i s/"#\$nrconf{kernelhints} = -1;"/"\$nrconf{kernelhints} = 0;"/g needrestart.conf;
+sed -i s/"#\$nrconf{restart} = 'i';"/"\$nrconf{restart} = 'a';"/g /etc/needrestart/needrestart.conf;
+sed -i s/"#\$nrconf{kernelhints} = -1;"/"\$nrconf{kernelhints} = 0;"/g /etc/needrestart/needrestart.conf;
 sudo apt upgrade -y;
 sudo apt install -y git;
 sudo apt install -y nginx;
