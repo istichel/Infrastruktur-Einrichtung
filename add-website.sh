bec76a52cb17ec438f4a77f32fd1895d998ea21f
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2023-03-21 14:35:09 +0100

    Update add-website.sh

--- add-website.sh
+++ add-website.sh
@@ -3,12 +3,12 @@ echo "Bitte gib den Pfad ein, indem sich die index.html Datei befindet (z.B. /ho
 read -p "Pfad: " pfad;
 echo "Bitte gib den Domainnamen ein, über den die Homepage erreichbar sein soll (z.B. schule.de)."
 read -p "Domain: " subdomain;
-
+chmod -R g+x $pfad;
 
 rm /etc/nginx/sites-enabled/$subdomain;
 echo "server {" >> /etc/nginx/sites-enabled/$subdomain;
 echo "server_name $subdomain;" >> /etc/nginx/sites-enabled/$subdomain;
-echo "root /var/www/html;" >> /etc/nginx/sites-enabled/$subdomain;
+echo "root $pfad;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "index index.html index.htm index.nginx-debian.html;" >> /etc/nginx/sites-enabled/$subdomain;
 echo "location / {" >> /etc/nginx/sites-enabled/$subdomain;
 echo "try_files \$uri \$uri/ =404;" >> /etc/nginx/sites-enabled/$subdomain;
